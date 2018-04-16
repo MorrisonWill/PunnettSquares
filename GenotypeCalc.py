5e1ce0f09b8f56c18c05424120e972cb3a9200ca
PontyMython1 <Matan_PDS@Cynthias-MBP.fios-router.home>
2018-04-15 22:59:20 -0400

    Function for foil, distribute, and get all combinations for each parent

--- GenotypeCalc.py
+++ GenotypeCalc.py
@@ -5,22 +5,46 @@ genotype1 = str(input("Input the first genotype here!"))
 genotype2 = str(input("Input the second genotype here!"))
 split_genes_list1 = []
 split_genes_list2 = []
+possible_genotype_list1 = []
+possible_genotype_list2 = []
 genotype_list=[]
 genotype_offspring=[]
 
-def make_strings_lists():
+def make_strings_lists(parent_genotype, split_genes_list):
     index1 = 0
     index2 = 1
     while index2 < len(genotype1):
-        split_genes_list1.append(genotype1[index1]+genotype1[index2])
-        split_genes_list2.append(genotype2[index1]+genotype1[index2])
+        split_genes_list.append(parent_genotype[index1]+parent_genotype[index2])
         index1+=2
         index2+=2
-    return(split_genes_list1)
 
-def get_all_combinations(parent):
-    make_strings_lists()
+def foil(first_gene, second_gene, list_to_store_results):
+    list_to_store_results.append(first_gene[0] + second_gene[0])
+    list_to_store_results.append(first_gene[0] + second_gene[1])
+    list_to_store_results.append(first_gene[1] + second_gene[0])
+    list_to_store_results.append(first_gene[1] + second_gene[1])
+    
+def distribute(gene, list_to_store_results): #list of current genotype possibilities
+    list_to_store_results.extend(list_to_store_results) # duplicate the list
+    for i in range(0, int(len(list_to_store_results)/2)):
+        list_to_store_results[i]+=gene[0]
+    for i in range(0, int(len(list_to_store_results)/2)):
+        list_to_store_results[int(len(list_to_store_results)/2) + i] += gene[1]
 
+
+def get_all_combinations(parent, split_genes_list, list_to_store_results): #get all combinations of each parent
+    make_strings_lists(parent, split_genes_list)
+    foil(split_genes_list[0], split_genes_list[1], list_to_store_results) #foil the first two binomiasls
+    if len(split_genes_list) > 2: #if there are more than two binomials...
+        for i in range (0, int(len(genotype1)/2)-2): #Loop the following the integer of half of the length of a parent's genotype
+            distribute(split_genes_list[2+i], list_to_store_results)
+
+# TESTING............
+#get_all_combinations(genotype1, split_genes_list1, possible_genotype_list1)
+#get_all_combinations(genotype2, split_genes_list2, possible_genotype_list2)
+#print(possible_genotype_list1)
+#print(possible_genotype_list2)
+    
 
             
 
